565eaa70c5f1a2cd723b3965886398ce88d33610
Avval Narang <96081283+itsTodFod@users.noreply.github.com>
2023-09-05 06:18:58 +0530

    Render Threads on Home

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,31 @@ export async function createThread({text, author, communityId, path} : Params) {
         throw new Error(`Error Creating Thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetching posts without parents as having parent makes it a comment of parent thread
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]} })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User})
+    .populate({
+        path: 'children',
+        populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined]} })
+
+    const posts = await postsQuery.exec();
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
+}
